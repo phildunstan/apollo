0f5aef78b03379fc345e568d5746bcbfbed0935c
Philip Dunstan <phil@philipdunstan.com>
2015-09-13 22:46:24 +0200

    Added AIModels to the recording snapshot.

--- apollo/recording.cpp
+++ apollo/recording.cpp
@@ -1,5 +1,6 @@
 #include "recording.h"
 
+#include <algorithm>
 #include <cassert>
 #include <memory>
 #include <vector>
@@ -17,12 +18,17 @@ struct Snapshot
 {
 	Time time;
 	uint64_t seed;
+
 	PlayerInput playerInput;
+
 	GameObject player;
 	vector<GameObject> aliens;
 	vector<GameObject> bullets;
+
 	vector<RigidBody> rigidBodies;
 	vector<CollisionObject> collisionObjects;
+
+	vector<std::unique_ptr<AIModel>> aiModels;
 };
 
 vector<Snapshot> snapshots;
@@ -37,6 +43,7 @@ int CreateSnapshot(Time frameTime, uint64_t frameSeed, const PlayerInput& player
 
 	snapshot.time = frameTime;
 	snapshot.seed = frameSeed;
+
 	snapshot.playerInput = playerInput;
 
 	snapshot.player = player;
@@ -46,7 +53,32 @@ int CreateSnapshot(Time frameTime, uint64_t frameSeed, const PlayerInput& player
 	snapshot.rigidBodies = rigidBodies;
 	snapshot.collisionObjects = collisionObjects;
 
+	snapshot.aiModels.resize(aiModels.size());
+	transform(cbegin(aiModels), cend(aiModels), begin(snapshot.aiModels), [] (const unique_ptr<AIModel>& aiModelPtr) { return (aiModelPtr ? aiModelPtr->Clone() : unique_ptr<AIModel>()); });
+
 	return static_cast<int>(snapshots.size() - 1);
+}
+
+void ReplaySnapshot(int snapshotIndex, Time& frameTime, uint64_t& frameSeed, PlayerInput& playerInput)
+{
+	PROFILER_TIMER_FUNCTION();
+	assert((snapshotIndex >= 0) && (snapshotIndex < snapshots.size()));
+	const auto& snapshot = snapshots[snapshotIndex];
+
+	frameTime = snapshot.time;
+	frameSeed = snapshot.seed;
+
+	playerInput = snapshot.playerInput;
+
+	player = snapshot.player;
+	aliens = snapshot.aliens;
+	bullets = snapshot.bullets;
+
+	rigidBodies = snapshot.rigidBodies;
+	collisionObjects = snapshot.collisionObjects;
+
+	aiModels.resize(snapshot.aiModels.size());
+	transform(cbegin(snapshot.aiModels), cend(snapshot.aiModels), begin(aiModels), [] (const unique_ptr<AIModel>& aiModelPtr) { return (aiModelPtr ? aiModelPtr->Clone() : unique_ptr<AIModel>()); });
 }
 
 int GetSnapshotCount()
@@ -59,30 +91,17 @@ void ValidateSnapshot(int snapshotIndex, Time frameTime, uint64_t frameSeed)
 	PROFILER_TIMER_FUNCTION();
 	assert((snapshotIndex >= 0) && (snapshotIndex < snapshots.size()));
 	const auto& snapshot = snapshots[snapshotIndex];
+
 	assert(snapshot.time == frameTime);
 	assert(snapshot.seed == frameSeed);
+
 	assert(snapshot.player.objectId == player.objectId);
 	assert(snapshot.aliens.size() == aliens.size());
 	assert(snapshot.bullets.size() == bullets.size());
+
 	assert(snapshot.rigidBodies.size() == rigidBodies.size());
 	assert(snapshot.collisionObjects.size() == collisionObjects.size());
-}
 
-void ReplaySnapshot(int snapshotIndex, Time& frameTime, uint64_t& frameSeed, PlayerInput& playerInput)
-{
-	PROFILER_TIMER_FUNCTION();
-	assert((snapshotIndex >= 0) && (snapshotIndex < snapshots.size()));
-	const auto& snapshot = snapshots[snapshotIndex];
-
-	frameTime = snapshot.time;
-	frameSeed = snapshot.seed;
-	playerInput = snapshot.playerInput;
-
-	player = snapshot.player;
-	aliens = snapshot.aliens;
-	bullets = snapshot.bullets;
-
-	rigidBodies = snapshot.rigidBodies;
-	collisionObjects = snapshot.collisionObjects;
+	assert(snapshot.aiModels.size() == aiModels.size());
 }
 
--- apollo/recording.h
+++ apollo/recording.h
@@ -8,9 +8,10 @@ struct PlayerInput;
 
 int CreateSnapshot(Time frameTime, uint64_t frameSeed, const PlayerInput& playerInput);
 
+void ReplaySnapshot(int snapshotIndex, Time& frameTime, uint64_t& frameSeed, PlayerInput& playerInput);
+
 int GetSnapshotCount();
 
 void ValidateSnapshot(int snapshotIndex, Time frameTime, uint64_t frameSeed);
 
-void ReplaySnapshot(int snapshotIndex, Time& frameTime, uint64_t& frameSeed, PlayerInput& playerInput);
 
